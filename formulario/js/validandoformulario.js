a097b577233b0b97903196a223df0ce2f3f61076
Aldeir Francisco <dide123francisco@gmail.com>
2021-02-01 18:27:14 -0300

    desabilitando butao

--- formulario/js/validandoformulario.js
+++ formulario/js/validandoformulario.js
@@ -1,31 +1,48 @@
-window.onload =function () {
+(window.onload =function () {
     'use strict';
-    const textTituluo = document.getElementById("txtTitulo");
-    const btn = document.getElementById("btn");
+    const textTituluo = document.getElementById('txtTitulo');
+    const btn = document.getElementById('btn');
     const formCadastro = document.querySelector(".formCadastro")
     // btn.addEventListener("click",function(e){
 
     // })
      formCadastro.addEventListener('submit', function (e) {
         if(!textTituluo.value){
-            alert("preencha todos os campos")
+           // showErroMessage("preencha todos os campos")
             e.preventDefault()// para o emvio do formulario
             textTituluo.focus()
         }
     })
-    const textDescricao = document.getElementById("txtDescricao")
-    const contadorContainer = document.getElementById("contador")
+   
+    const textDescricao = document.getElementById('txtDescricao')
+    
+    const contadorContainer = document.getElementById('contador')
     const resta = contadorContainer.getElementsByTagName("span")[0]
-    const maxima = textDescricao.
+    const maxima = textDescricao.maxLength
+      mostrandoNumero(maxima)
     // contadorContainer.removeAttribute("style")
-    contadorContainer.style.display ="block"
+    contadorContainer.style.display ='block'
     
     function  checkLength(){
         let numeroLetraDigitada = this.value.length
           let caracteres = parseInt(maxima)- parseInt(numeroLetraDigitada)
-          resta.textContent = caracteres
-       
-    })
-
-    textDescricao.addEventListener("input",checkLength)
-}
+         
+       mostrandoNumero(caracteres)
+    }
+       textDescricao.addEventListener("input",checkLength)
+       function mostrandoNumero(n){
+       resta.textContent = n
+       }
+       console.log(btn)
+    btn.disabled = true
+    const chkAceito = document.getElementById('chkAceito')
+     chkAceito.addEventListener("change",function(){
+         btn.disabled = !this.checked
+     })
+    // const feedbackMessage = document.getElementById('feedbackMessage')
+    // const feedBackMessageCloseBtn = feedbackMessage
+    //  function  showErroMessage(msn){
+    //     feedbackMessage.classList.add("show")
+    //     feedbackMessage.getElementsByTagName("p")[0].textContent = msn;
+    // }
+})()      
